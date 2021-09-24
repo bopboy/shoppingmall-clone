ab9d1fa0296fa9d1f8b4966dc16d13f9d5d4d4b9
bopboy2 <scp0000@gmail.com>
2021-09-24 16:18:23 +0900

    이미지 슬라이더 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
 import { Icon, Col, Card, Row } from 'antd'
+import ImageSlider from '../../utils/ImageSlider';
+
 const { Meta } = Card;
 
 function LandingPage() {
@@ -17,7 +19,7 @@ function LandingPage() {
         return (
             <Col lg={6} md={8} xs={24} key={index}>
                 <Card
-                    cover={<img src={`http://localhost:5000/${product.images[0]}`} style={{ width: '100%', maxHeight: '150px' }} />}
+                    cover={<ImageSlider images={product.images} />}
                 >
                     <Meta
                         title={product.title}
