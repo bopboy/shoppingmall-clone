74f75cacdd1fe16e5cc3eb5ce04ec5d073e299d7
bopboy2 <scp0000@gmail.com>
2021-09-28 19:56:28 +0900

    결제 성공  후 작업(DB 업데이트, 화면 업데이트  등)

--- client/src/components/views/ CartPage/CartPage.js
+++ client/src/components/views/ CartPage/CartPage.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { getCartItems } from '../../../_actions/user_actions'
-import { removeCartItem } from '../../../_actions/user_actions'
+import { getCartItems, removeCartItem, onSuccessBuy } from '../../../_actions/user_actions'
 import UserCardBlock from './Sections/UserCardBlock'
 import Paypal from '../../utils/PayPal'
-import { Empty } from 'antd'
+import { Empty, Result } from 'antd'
 
 function CartPage(props) {
     const [Total, setTotal] = useState(0)
     const [ShowTotal, setShowTotal] = useState(false)
+    const [ShowSuccess, setShowSuccess] = useState(false)
     const dispatch = useDispatch()
     useEffect(() => {
         let cartItems = []
@@ -31,6 +31,15 @@ function CartPage(props) {
                 if (res.payload.productInfo.length <= 0) setShowTotal(false)
             })
     }
+    const transactionSuccess = (data) => {
+        dispatch(onSuccessBuy({ paymentData: data, cartDetail: props.user.cartDetail }))
+            .then(res => {
+                if (res.payload.success) {
+                    setShowTotal(false)
+                    setShowSuccess(true)
+                }
+            })
+    }
     return (
         <div style={{ width: '85%', margin: '3rem auto' }}>
             <h1>장바구니</h1>
@@ -41,14 +50,16 @@ function CartPage(props) {
                 <div style={{ marginTop: '3rem' }}>
                     <h2>총 합계: ${Total}</h2>
                 </div>
-                :
-                <>
-                    <br />
-                    <Empty description={false} />
-                </>
+                : ShowSuccess ?
+                    <Result status="success" title="Successfully Purchased Items" />
+                    :
+                    <>
+                        <br />
+                        <Empty description={false} />
+                    </>
             }
             {ShowTotal ?
-                <Paypal total={Total} />
+                <Paypal total={Total} onSuccess={transactionSuccess} />
                 : <p>No items in the Cart</p>
             }
         </div>
